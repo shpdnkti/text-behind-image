d03c36ffd74118d0721149ef219e2e84b2b22848
NULL <kamwlung@gmail.com>
2024-11-04 17:26:38 +0000

    da

--- components/authenticate.tsx
+++ components/authenticate.tsx
@@ -15,9 +15,9 @@ const Authenticate = () => {
         process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
         'http://localhost:3000/'
       // Make sure to include `https://` when not localhost.
-      url = url.startsWith('http') ? url : `https://${url}/app`
+      url = url.startsWith('http') ? url : `https://${url}`
       // Make sure to include a trailing `/`.
-      url = url.endsWith('/') ? url : `${url}/app`
+      url = url.endsWith('/') ? url : `${url}/`
       return url
     }
     
